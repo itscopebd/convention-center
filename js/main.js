1c9c6ac1d36f8e5b7ae1d5347dacb6d7f189709d
itscopebd <itscopebd@gmail.com>
2023-05-28 22:18:27 +0600

    Modify all files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// let toast__wrap=document.getElementById("toast__wrap");
+
+function customToast(){
+    document.getElementById("toastWrap").style.display="none";
+    console.log("i am call")
+}
